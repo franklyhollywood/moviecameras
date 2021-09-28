dedbf7185bc2014d431d94c439b5e9fa920c6be8
Frank Hollywood <github@srvsync.com>
2021-09-28 13:21:20 -0700

    started project

---
+++ data.js
@@ -0,0 +1,39 @@
+{
+"id": 1,
+"make": "Beaulieu",
+"model": "9008S",
+"year_made": 1993,
+"sound": true
+}
+
+{
+"id": 2,
+"make": "Cannon",
+"model": "1014XLS",
+"year_made": 1979,
+"sound": true
+}
+
+{
+"id": 3,
+"make": "Elmo",
+"model": "1012SXL",
+"year_made": 1978,
+"sound": true
+}
+
+{
+"id": 4,
+"make": "Leicina",
+"model": "Special",
+"year_made": 1972,
+"sound": true
+}
+
+{
+"id": 5,
+"make": "Nizo",
+"model": "6080",
+"year_made": 1980,
+"sound": true
+} 
